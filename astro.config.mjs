898a8a187ba767a5e5e7466fa31422653ffd4f58
Addiel Lucena <addiellucena94@gmail.com>
2026-01-22 18:54:20 -0300

    High deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,16 +3,21 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
+
   i18n: {
     locales: ["es", "en", "pt-br", "nl-nl"],
     defaultLocale: "en",
     routing: {
         prefixDefaultLocale: false
     }
-  }
+  },
+  site: 'https://addevnv.com',
+  integrations: [sitemap()]
 });
